ef063f7f621f10a404b3a0117263e65cef842a5b
ArjunSingh2005 <mc.dragonwolf@gmail.com>
2025-05-07 06:35:25 -0700

    Add files via upload

---
+++ RPG/CMakeFiles/RPGGame.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/data/home/csmajs/hzhan407/final-project-cs100_sec023_projectgroup8/src/Characters.cpp" "CMakeFiles/RPGGame.dir/src/Characters.cpp.o" "gcc" "CMakeFiles/RPGGame.dir/src/Characters.cpp.o.d"
+  "/data/home/csmajs/hzhan407/final-project-cs100_sec023_projectgroup8/src/Combat.cpp" "CMakeFiles/RPGGame.dir/src/Combat.cpp.o" "gcc" "CMakeFiles/RPGGame.dir/src/Combat.cpp.o.d"
+  "/data/home/csmajs/hzhan407/final-project-cs100_sec023_projectgroup8/src/NPC.cpp" "CMakeFiles/RPGGame.dir/src/NPC.cpp.o" "gcc" "CMakeFiles/RPGGame.dir/src/NPC.cpp.o.d"
+  "/data/home/csmajs/hzhan407/final-project-cs100_sec023_projectgroup8/src/Player.cpp" "CMakeFiles/RPGGame.dir/src/Player.cpp.o" "gcc" "CMakeFiles/RPGGame.dir/src/Player.cpp.o.d"
+  "/data/home/csmajs/hzhan407/final-project-cs100_sec023_projectgroup8/src/game_screens.cpp" "CMakeFiles/RPGGame.dir/src/game_screens.cpp.o" "gcc" "CMakeFiles/RPGGame.dir/src/game_screens.cpp.o.d"
+  "/data/home/csmajs/hzhan407/final-project-cs100_sec023_projectgroup8/src/main.cpp" "CMakeFiles/RPGGame.dir/src/main.cpp.o" "gcc" "CMakeFiles/RPGGame.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ RPG/CMakeFiles/RPGGame.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RPGGame.dir/src/Characters.cpp.o"
+  "CMakeFiles/RPGGame.dir/src/Characters.cpp.o.d"
+  "CMakeFiles/RPGGame.dir/src/Combat.cpp.o"
+  "CMakeFiles/RPGGame.dir/src/Combat.cpp.o.d"
+  "CMakeFiles/RPGGame.dir/src/NPC.cpp.o"
+  "CMakeFiles/RPGGame.dir/src/NPC.cpp.o.d"
+  "CMakeFiles/RPGGame.dir/src/Player.cpp.o"
+  "CMakeFiles/RPGGame.dir/src/Player.cpp.o.d"
+  "CMakeFiles/RPGGame.dir/src/game_screens.cpp.o"
+  "CMakeFiles/RPGGame.dir/src/game_screens.cpp.o.d"
+  "CMakeFiles/RPGGame.dir/src/main.cpp.o"
+  "CMakeFiles/RPGGame.dir/src/main.cpp.o.d"
+  "RPGGame"
+  "RPGGame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RPGGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
